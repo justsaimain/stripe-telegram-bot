e1a8c626fa35e4a38d39f7c1757949d394fb23ed
Sai Main <saimain.venuslab@gmail.com>
2022-05-19 21:27:23 +0630

    init

--- index.js
+++ index.js
@@ -184,10 +184,13 @@ app.post(URI, async (req, res) => {
 mongoose
   .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(function () {
-    app.listen(process.env.PORT || 5000, async () => {
-      console.log("App is running on port " + process.env.PORT || 5000);
-      // await deleteWebHook();
-      await init();
-    });
-  })
-  .catch((err) => console.log(err));
+    try {
+      app.listen(process.env.PORT || 5000, async () => {
+        console.log("App is running on port " + process.env.PORT || 5000);
+        // await deleteWebHook();
+        await init();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
