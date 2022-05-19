658692253c939f5c92741053effc7cc9df8ff812
Sai Main <saimain.venuslab@gmail.com>
2022-05-19 21:58:41 +0630

    init

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ app.post(URI, async (req, res) => {
     const token = splitText[1];
     const findToken = await TokenModel.findOne({ token: token });
     if (findToken) {
-      const updateLimit = findToken.data.limit - 1;
+      const updateLimit = findToken.data?.limit - 1;
       TokenModel.update({
         token: token,
         limit: updateLimit,
@@ -186,7 +186,7 @@ mongoose
   .then(function () {
     app.listen(process.env.PORT || 5000, async () => {
       console.log("App is running on port " + process.env.PORT || 5000);
-      // await deleteWebHook();
+      await deleteWebHook();
       await init();
     });
   })
